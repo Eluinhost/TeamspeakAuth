4bc83fefeef75c8c43c5f3b9eb519fe6ce356f6e
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-12 17:08:00 +0100

    remove TS3 api from repository

--- lib/AuthException.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Thrown when config file failed to load
- */
-class ConfigFileNotFoundException extends Exception{}
-/**
- * Thrown when config file was not valid json
- */
-class ConfigFileInvalidException extends Exception{}
