f16da10978e298f5572c0ec68930c7bb7a48804c
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-13 11:19:28 +0100

    start porting some things over from the Common file into a service type class

---
+++ src/com/publicuhc/ts3auth/Ts3Interface.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace com\publicuhc\ts3auth;
+
+use TeamSpeak3;
+use TeamSpeak3_Adapter_ServerQuery_Exception;
+use TeamSpeak3_Node_Client;
+use TeamSpeak3_Node_Server;
+
+class Ts3Interface {
+
+    private $host;
+    private $queryPort;
+    private $serverPort;
+    private $username;
+    private $password;
+
+    public function __construct($host, $queryPort, $serverPort, $username, $password = '') {
+        $this->host = $host;
+        $this->queryPort = $queryPort;
+        $this->serverPort = $serverPort;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * Return the UUID for the given name
+     * @param $name string the name to search for
+     * @return TeamSpeak3_Node_Client|null the client if found, null otherwise
+     */
+    public function getClientForName($name) {
+        $server = self::getServerInstance();
+        $client = null;
+        try{
+            $client = $server->clientGetByName($name);
+        }catch (TeamSpeak3_Adapter_ServerQuery_Exception $ignored){}
+
+        return $client;
+    }
+
+    public function getUUIDForClient(TeamSpeak3_Node_Client $client) {
+        return $client->infoDb()['client_unique_identifier'];
+    }
+
+    /**
+     * @return TeamSpeak3_Node_Server
+     */
+    public function getServerInstance() {
+        return TeamSpeak3::factory("serverquery://{$this->username}:{$this->password}@{$this->host}:{$this->queryPort}/?server_port={$this->serverPort}");
+    }
+} 
