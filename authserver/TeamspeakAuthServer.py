3cfa8d09912ae6e1884c4135dd39eeb9ee848e14
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-13 16:01:08 +0100

    read from config file

--- authserver/TeamspeakAuthServer.py
+++ authserver/TeamspeakAuthServer.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 import random
 import MySQLdb
-import json
+import yaml
 from authserver import AuthServer
 
-with open('./../config/config.json') as data_file:
-    data = json.load(data_file)
+with open('./../config/config.yml') as data_file:
+    data = yaml.load(data_file)
 
-HOST = data['mc']['host']
-PORT = data['mc']['port']
-MOTD = data['mc']['motd']
+MC_HOST = data['minecraft']['host']
+MC_PORT = data['minecraft']['port']
+MC_MOTD = data['minecraft']['motd']
+DB_HOST = data['database']['host']
+DB_PORT = data['database']['port']
+DB_USERNAME = data['database']['username']
+DB_PASSWORD = data['database']['password']
+DB_DATABASE = data['database']['database']
 FAVICON = 'servericon.png'  # path to a 64x64 .png
 
 
@@ -18,7 +23,12 @@ class TeamspeakAuthServer(AuthServer):
         print "%s/%s logged in" % (username, client_addr)
         if authed:
             print " --> OK!"
-            db = MySQLdb.connect(host=data['db']['host'],port=data['db']['port'], user=data['db']['username'], passwd=data['db']['password'], db=data['db']['database'])
+            db = MySQLdb.connect(
+                host=DB_HOST,
+                port=DB_PORT,
+                user=DB_USERNAME,
+                passwd=DB_PASSWORD,
+                db=DB_PASSWORD)
             cur = db.cursor()
             code = ''.join(random.choice('ABCDEF0123456789') for i in range(10))
             print " --> CODE "+code
@@ -29,13 +39,13 @@ class TeamspeakAuthServer(AuthServer):
             cur.close()
             db.commit()
             db.close()
-            return "Your code is \""+code+"\", it will last for 15 minutes."
+            return "Your code is %s it will last for 15 minutes." % code
         else:
             print " --> Failed login"
             return u"§4Couldn't authenticate you, please try again later"
 
 
 if __name__ == "__main__":
-    server = TeamspeakAuthServer(MOTD, FAVICON)
-    server.listen(HOST, PORT)
+    server = TeamspeakAuthServer(MC_MOTD, FAVICON)
+    server.listen(MC_HOST, MC_PORT)
     server.run()
