8ea0c67b5fd8ce399af6d1c05765f38795d5f86e
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-13 10:11:15 +0100

    use the kernel and routing to call the right action e.t.c.

--- web/app.php
+++ web/app.php
@@ -1,12 +1,33 @@
 <?php
 
-if (!defined('ROOT')) {
-    define('ROOT', dirname(dirname(__FILE__)));
-}
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\HttpKernel\EventListener\RouterListener;
+use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
 
-require_once ROOT . '/vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
-// look inside *this* directory
-$locator = new FileLocator(array(__DIR__));
+// look inside the src directory
+$locator = new FileLocator(array(__DIR__ . '/../src/'));
 $loader = new YamlFileLoader($locator);
 $collection = $loader->load('routes.yml');
+
+$request = Request::createFromGlobals();
+
+$matcher = new UrlMatcher($collection, new RequestContext());
+
+$dispatcher = new EventDispatcher();
+$dispatcher->addSubscriber(new RouterListener($matcher));
+
+$resolver = new ControllerResolver();
+$kernel = new HttpKernel($dispatcher, $resolver);
+
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
