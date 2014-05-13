ec636733685d9986c250dc42a3c454733b0fd012
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-13 11:19:57 +0100

    add teamspeak request, all except the adding to database

--- src/com/publicuhc/ts3auth/Ts3Auth.php
+++ src/com/publicuhc/ts3auth/Ts3Auth.php
@@ -2,6 +2,8 @@
 
 namespace com\publicuhc\ts3auth;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class Ts3Auth {
@@ -10,8 +12,40 @@ class Ts3Auth {
         //TODO
     }
 
-    public function requestTeamspeakAction() {
-        //TODO
+    public function requestTeamspeakAction(Request $request) {
+        $response = new JsonResponse();
+
+        $tsName = $request->query->get('ts_name');
+
+        if( $tsName == null || strlen($tsName) == 0 || strlen($tsName) > 30 ) {
+            $response->setStatusCode(400);
+            $response->setData([
+                'error' => 'Invalid teamspeak name provided'
+            ]);
+            return $response;
+        }
+
+        $ts3 = new Ts3Interface();
+
+        $client = $ts3->getClientForName($tsName);
+
+        if( null == $client ) {
+            $response->setStatusCode(400);
+            $response->setData([
+                'error' => 'Teamspeak user not found'
+            ]);
+            return $response;
+        }
+
+        $uuid = $ts3->getUUIDForClient($client);
+        $randomCode = substr(md5(rand()), 0, 10);
+
+        //TODO insert UUID and code into database
+
+        $response->setData(array(
+            'UUID' => $uuid
+        ));
+        return $response;
     }
 
     public function indexAction() {
