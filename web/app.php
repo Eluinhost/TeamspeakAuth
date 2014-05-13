8e11972b33ee92430ce90be1cc2034a70f370cf5
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-13 10:47:22 +0100

    load services (currently empty). Currently do nothing with the container

--- web/app.php
+++ web/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
@@ -17,7 +18,10 @@ $locator = new FileLocator(array(__DIR__ . '/../src/'));
 $loader = new YamlFileLoader($locator);
 $collection = $loader->load('routes.yml');
 
-$request = Request::createFromGlobals();
+$container = new ContainerBuilder();
+$diLoader = new FileLocator(__DIR__ . '/../src/');
+$diLoader = new Symfony\Component\DependencyInjection\Loader\YamlFileLoader($container, $diLoader);
+$diLoader->load('services.yml');
 
 $matcher = new UrlMatcher($collection, new RequestContext());
 
@@ -27,6 +31,7 @@ $dispatcher->addSubscriber(new RouterListener($matcher));
 $resolver = new ControllerResolver();
 $kernel = new HttpKernel($dispatcher, $resolver);
 
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 
