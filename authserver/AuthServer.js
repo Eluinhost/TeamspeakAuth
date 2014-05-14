0253437801152711d7affaa409fa04f66233b12f
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-14 11:45:55 +0100

    read from the config.yml file listen for connect and kick immediately

--- authserver/AuthServer.js
+++ authserver/AuthServer.js
@@ -1,12 +1,33 @@
 var mc = require('minecraft-protocol');
+var yaml = require('yaml');
+fs   = require('fs');
+
+var host = '0.0.0.0';
+var post = 25565;
+var motd = '§eAuth Server';
+
+try {
+    var doc = yaml.safeLoad(fs.readFileSync('./../config/config.yml', 'utf8'));
+    var parameters = doc.parameters.minecraft;
+    host = parameters.host;
+    port = parameters.port;
+    motd = parameters.motd;
+} catch (e) {
+    console.log(e);
+    return;
+}
 
 var server = mc.createServer({
-    'online-mode': true,   // optional
-    encryption: true,      // optional
-    host: '0.0.0.0',       // optional
-    port: 25565           // optional
+    'online-mode': true,
+    encryption: true,
+    host: host,
+    port: port,
+    motd: motd
 });
 
 server.on('login', function(client) {
-
+    //TODO generate code, insert into database send player the code
+    client.write('kick_disconnect', {
+        reason: 'Kick message'
+    });
 });
