ff015540f91b046d91bf24c1591538ccb6fafc21
HowdenG <graham_howden1@yahoo.co.uk>
2014-05-15 11:46:41 +0100

    add a minecraft helper with a way to get a head icon by the username

---
+++ src/PublicUHC/TeamspeakAuth/Helpers/DefaultMinecraftHelper.php
@@ -0,0 +1,18 @@
+<?php
+namespace PublicUHC\TeamspeakAuth\Helpers;
+
+
+class DefaultMinecraftHelper implements MinecraftHelper {
+
+    private $url;
+
+    public function __construct($url) {
+        $this->url = $url;
+    }
+
+    public function getIconForUsername($username)
+    {
+        $iconURL = str_replace("%username%", $username, $this->url);
+        return file_get_contents($iconURL);
+    }
+}
