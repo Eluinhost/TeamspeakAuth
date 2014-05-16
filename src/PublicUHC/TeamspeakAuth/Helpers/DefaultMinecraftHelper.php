42706dba595897b3f7d8cff0df16247ca9da2394
Graham <graham_howden1@yahoo.co.uk>
2014-05-16 23:05:56 +0100

    use ; instead of % to avoid confusing the DI container

--- src/PublicUHC/TeamspeakAuth/Helpers/DefaultMinecraftHelper.php
+++ src/PublicUHC/TeamspeakAuth/Helpers/DefaultMinecraftHelper.php
@@ -12,7 +12,7 @@ class DefaultMinecraftHelper implements MinecraftHelper {
 
     public function getIconForUsername($username)
     {
-        $iconURL = str_replace("%username%", $username, $this->url);
+        $iconURL = str_replace(";username;", $username, $this->url);
         return file_get_contents($iconURL);
     }
 }
