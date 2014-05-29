81209a1973836a6ad991bc62f092c9aa4d360478
Graham <graham_howden1@yahoo.co.uk>
2014-05-29 19:55:31 +0100

    fill out the models for teamspeak accounts and their code

--- src/PublicUHC/TeamspeakAuth/Entities/TeamspeakAccount.php
+++ src/PublicUHC/TeamspeakAuth/Entities/TeamspeakAccount.php
@@ -2,7 +2,60 @@
 
 namespace PublicUHC\TeamspeakAuth\Entities;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\OneToMany;
 
+/**
+ * @Entity
+ * @Table(name="teamspeakaccounts")
+ */
 class TeamspeakAccount {
 
-} 
+    use DatabaseTrait;
+
+    /**
+     * @Column(type="guid")
+     */
+    private $uuid;
+
+    /**
+     * @Column(type="string")
+     */
+    private $name;
+
+    /**
+     * @OneToMany(targetEntity="PublicUHC\TeamspeakAuth\Entities\TeamspeakCode", mappedBy="account", orphanRemoval=true)
+     * @var $codes ArrayCollection
+     */
+    private $codes;
+
+    public function __construct()
+    {
+        $this->codes = new ArrayCollection();
+    }
+
+    public function getUUID() {
+        return $this->uuid;
+    }
+
+    public function setUUID($uuid) {
+        $this->uuid = $uuid;
+        return $this;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getCodes() {
+        return $this->codes;
+    }
+}
--- src/PublicUHC/TeamspeakAuth/Entities/TeamspeakCode.php
+++ src/PublicUHC/TeamspeakAuth/Entities/TeamspeakCode.php
@@ -3,6 +3,50 @@
 namespace PublicUHC\TeamspeakAuth\Entities;
 
 
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\JoinColumn;
+
+/**
+ * @Entity
+ * @Table(name="teamspeakcodes")
+ */
 class TeamspeakCode {
 
+    use DatabaseTrait;
+
+    /**
+     * @Column(type="string")
+     */
+    private $code;
+
+    /**
+     * @ManyToOne(targetEntity="PublicUHC\TeamspeakAuth\Entities\TeamspeakAccount", inversedBy="codes")
+     * @JoinColumn(name="TeamspeakAccountId")
+     */
+    private $account;
+
+    public function __construct() {
+        $this->code = substr(md5(rand()), 0, 10);
+    }
+
+    public function getCode() {
+        return $this->code;
+    }
+
+    public function setCode($code) {
+        $this->code = $code;
+        return $this;
+    }
+
+    public function getAccount() {
+        return $this->account;
+    }
+
+    public function setAccount(TeamspeakAccount $account) {
+        $this->account = $account;
+        return $this;
+    }
 } 
