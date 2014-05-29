30744bb228c2e287ff387e7289ea0e120cd783ba
Graham <graham_howden1@yahoo.co.uk>
2014-05-29 14:48:39 +0100

    add src directory to parameters

--- web/app.php
+++ web/app.php
@@ -45,6 +45,7 @@ $collection = $loader->load('routes.yml');
  * Load the DI container from the file /config/config.yml
  */
 $container = new ContainerBuilder(new ParameterBagNested());
+$container->setParameter('global.srcroot', __DIR__ . '/../src/');
 $diLoader = new Symfony\Component\DependencyInjection\Loader\YamlFileLoader($container, $configLocator);
 $diLoader->load('config.yml');
 
