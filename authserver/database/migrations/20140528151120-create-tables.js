b504e6bdd9af27e1b59189816fcb138d4e9c2978
Graham <graham_howden1@yahoo.co.uk>
2014-05-29 11:43:43 +0100

    need to handle table prefixes in keyed columns

--- authserver/database/migrations/20140528151120-create-tables.js
+++ authserver/database/migrations/20140528151120-create-tables.js
@@ -2,152 +2,162 @@ var async = require('async'),
     config = require('./../../config/config');
 
 var setupAuthenticationsTable = function(migration, DataTypes) {
+    var AuthenticationsTable = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    };
+    AuthenticationsTable[config.database.tablePrefix + 'MinecraftAccountId'] = {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    };
+    AuthenticationsTable[config.database.tablePrefix + 'TeamspeakAccountId'] = {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    };
+
     migration.createTable(
         config.database.tablePrefix + 'authentications',
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            MinecraftAccountId: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            TeamspeakAccountId: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            }
-        }
+        AuthenticationsTable
     );
 };
 
 var setupMinecraftAccountsTable = function(migration, DataTypes) {
+    var MinecraftAccountsTable = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        uuid: {
+            type: DataTypes.UUID,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(16),
+            allowNull: false
+        }
+    };
     migration.createTable(
         config.database.tablePrefix + 'minecraftaccounts',
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            uuid: {
-                type: DataTypes.UUID,
-                allowNull: false
-            },
-            name: {
-                type: DataTypes.STRING(16),
-                allowNull: false
-            }
-        }
+        MinecraftAccountsTable
     );
 };
 
 var setupTeamspeakAccountsTable = function(migration, DataTypes) {
+    var TeamspeakAccountsTable = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        uuid: {
+            type: DataTypes.UUID,
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(30),
+            allowNull: false
+        }
+    };
     migration.createTable(
         config.database.tablePrefix + 'teamspeakaccounts',
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            uuid: {
-                type: DataTypes.UUID,
-                allowNull: false
-            },
-            name: {
-                type: DataTypes.STRING(30),
-                allowNull: false
-            }
-        }
+        TeamspeakAccountsTable
     );
 };
 
 var setupMinecraftCodesTable = function(migration, DataTypes) {
+    var MinecraftCodesTable = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        code: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        }
+    };
+    MinecraftCodesTable[config.database.tablePrefix + 'MinecraftAccountId'] = {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    };
     migration.createTable(
         config.database.tablePrefix + 'minecraftcodes',
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            code: {
-                type: DataTypes.STRING(10),
-                allowNull: false
-            },
-            MinecraftAccountId: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            }
-        }
+        MinecraftCodesTable
     );
 };
 
 var setupTeamspeakCodesTable = function(migration, DataTypes) {
+    var TeamspeakCodesTable = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        code: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        TeamspeakAccountId: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    };
+    TeamspeakCodesTable[config.database.tablePrefix + 'TeamspeakAccountId'] = {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    };
     migration.createTable(
         config.database.tablePrefix + 'teamspeakcodes',
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-                allowNull: false
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: false
-            },
-            code: {
-                type: DataTypes.STRING(10),
-                allowNull: false
-            },
-            TeamspeakAccountId: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            }
-        }
+        TeamspeakCodesTable
     );
 };
 
