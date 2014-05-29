e2948110ba537b98d6395bccc50faa177ae409f5
Graham <graham_howden1@yahoo.co.uk>
2014-05-29 15:00:14 +0100

    revert the addition of tablePrefixes

--- authserver/database/models/MinecraftAccount.js
+++ authserver/database/models/MinecraftAccount.js
@@ -1,14 +1,6 @@
-var config = require('./../../config/config');
-
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define(
-        'MinecraftAccount',
-        {
-            uuid: DataTypes.UUID,
-            name: DataTypes.STRING(16)
-        },
-        {
-            tableName: config.database.tablePrefix + 'minecraftaccounts'
-        }
-    );
+    return sequelize.define('MinecraftAccount', {
+        uuid: DataTypes.UUID,
+        name: DataTypes.STRING(16)
+    });
 };
--- authserver/database/models/TeamspeakAccount.js
+++ authserver/database/models/TeamspeakAccount.js
@@ -1,14 +1,6 @@
-var config = require('./../../config/config');
-
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define(
-        'TeamspeakAccount',
-        {
-            uuid: DataTypes.UUID,
-            name: DataTypes.STRING(30)
-        },
-        {
-            tableName: config.database.tablePrefix + 'teamspeakaccounts'
-        }
-    );
+    return sequelize.define('TeamspeakAccount', {
+        uuid: DataTypes.UUID,
+        name: DataTypes.STRING(30)
+    });
 };
