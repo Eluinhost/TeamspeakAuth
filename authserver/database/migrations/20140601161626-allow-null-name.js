4303146e280b5490d951cdd9b0eacd6e0fea0a70
Graham <graham_howden1@yahoo.co.uk>
2014-06-01 16:40:50 +0100

    add migration to allow null as a minecraft name

---
+++ authserver/database/migrations/20140601161626-allow-null-name.js
@@ -0,0 +1,26 @@
+module.exports = {
+    up: function(migration, DataTypes, done) {
+        migration.changeColumn(
+            'MinecraftAccounts',
+            'name',
+            {
+                type: DataTypes.STRING(16),
+                allowNull: true
+            }
+        ).success(function() {
+            done();
+        });
+    },
+    down: function(migration, DataTypes, done) {
+        migration.changeColumn(
+            'MinecraftAccounts',
+            'name',
+            {
+                type: DataTypes.STRING(16),
+                allowNull: false
+            }
+        ).success(function() {
+            done();
+        });
+    }
+};
