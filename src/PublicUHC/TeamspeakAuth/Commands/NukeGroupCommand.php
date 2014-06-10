6191fff90fabc1116f52b06f38b572a748b9d534
Graham <graham_howden1@yahoo.co.uk>
2014-06-10 21:22:29 +0100

    start of a nuke group command

---
+++ src/PublicUHC/TeamspeakAuth/Commands/NukeGroupCommand.php
@@ -0,0 +1,37 @@
+<?php
+namespace PublicUHC\TeamspeakAuth\Commands;
+
+use PublicUHC\TeamspeakAuth\Helpers\TeamspeakHelper;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NukeGroupCommand extends Command {
+
+    private $teamspeakHelper;
+
+    public function __construct(TeamspeakHelper $teamspeakHelper) {
+        $this->teamspeakHelper = $teamspeakHelper;
+    }
+
+    protected function configure()
+    {
+        $this->setName('nuke:group')
+            ->setDescription('Nuke the group with the given ID')
+            ->addArgument(
+                'groupID',
+                InputArgument::REQUIRED,
+                'What is the ID of the group you want to nuke?'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $groupID = $input->getArgument('groupID');
+
+
+
+        $output->writeln($groupID);
+    }
+}
