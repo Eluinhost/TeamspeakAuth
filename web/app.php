509a109098fc9e8ca953909a84e8c267df4af22c
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-10 12:05:27 +0100

    initialize everything with the new DI. Container is cached and so is the routing

--- web/app.php
+++ web/app.php
@@ -1,89 +1,46 @@
 <?php
 
-use PublicUHC\TeamspeakAuth\Extensions\AssetExtension;
-use PublicUHC\TeamspeakAuth\ControllerResolver;
+use PublicUHC\TeamspeakAuth\Container\ProjectContainer;
 use PublicUHC\TeamspeakAuth\ParameterBagNested;
-use Symfony\Bridge\Twig\Extension\RoutingExtension;
-use Symfony\Bridge\Twig\TwigEngine;
+use Symfony\Component\Config\ConfigCache;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\EventListener\RouterListener;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\HttpKernel;
-use Symfony\Component\Routing\Generator\UrlGenerator;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Templating\Asset\PathPackage;
-use Symfony\Component\Templating\Helper\AssetsHelper;
-use Symfony\Component\Templating\TemplateNameParser;
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
-/**
- * Create the request and it's context
- */
 $request = Request::createFromGlobals();
+$isDebug = false;
+$projectRoot = dirname(dirname(__FILE__));
 
-$requestContext = new RequestContext();
-$requestContext->fromRequest($request);
+$containerFile = $projectRoot . '/cache/container/ProjectContainer.php';
+$containerConfigCache = new ConfigCache($containerFile, $isDebug);
 
-/**
- * Set up the file locator to get our config files from
- */
-$configLocator = new FileLocator(__DIR__ . '/../config/');
+if (!$containerConfigCache->isFresh()) {
+    $container = new ContainerBuilder(new ParameterBagNested());
+    $container->setParameter('global.root', $projectRoot);
 
-/*
- * Load the routing from the file /config/routes.yml
- */
-$loader = new Symfony\Component\Routing\Loader\YamlFileLoader($configLocator);
-$collection = $loader->load('routes.yml');
+    $configLocator = new FileLocator($projectRoot . '/config/');
+    $diLoader = new YamlFileLoader($container, $configLocator);
+    $diLoader->load('config.yml');
 
-/*
- * Load the DI container from the file /config/config.yml
- */
-$container = new ContainerBuilder(new ParameterBagNested());
-$container->setParameter('global.root', __DIR__ . '/..');
-$diLoader = new Symfony\Component\DependencyInjection\Loader\YamlFileLoader($container, $configLocator);
-$diLoader->load('config.yml');
+    $container->compile();
 
-/**
- * Set up twig and add it to the container
- */
-$twigEnvironment = new Twig_Environment(new Twig_Loader_Filesystem(__DIR__ . '/../templates/'), ['cache' => __DIR__ . '/../cache/templates']);
-$twigEnvironment->addExtension(new RoutingExtension(new UrlGenerator($collection, $requestContext)));
-$templating = new TwigEngine($twigEnvironment, new TemplateNameParser());
-$container->set('templating', $templating);
-
-/**
- * Set up asset extension
- */
-$assetsHelper = new AssetsHelper($request->getBasePath());
-
-$assetsHelper->addPackage('vendor', new PathPackage('vendor'));
-
-$assetsExtension = new AssetExtension($assetsHelper);
-$twigEnvironment->addExtension($assetsExtension);
-
-/**
- * Set up the kernel
- */
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new RouterListener(new UrlMatcher($collection, $requestContext)));
-$kernel = new HttpKernel($dispatcher, new ControllerResolver($container));
-
-/**
- * Handle the request
- */
-try {
-    $response = $kernel->handle($request);
-} catch (NotFoundHttpException $ex) {
-    $response = new Response($templating->render('404.html.twig'), 404);
-} catch(Exception $ex) {
-    error_log('Internal Error: ' . $ex->getMessage());
-    $response = new Response($templating->render('500.html.twig'), 500);
+    $dumper = new PhpDumper($container);
+    $containerConfigCache->write(
+        $dumper->dump([
+            'class' => 'ProjectContainer',
+            'namespace' => 'PublicUHC\TeamspeakAuth\Container'
+        ]),
+        $container->getResources()
+    );
 }
+
+require_once $containerFile;
+
+$container = new ProjectContainer();
+
+$response = $container->get('framework')->handle($request);
 $response->send();
-$kernel->terminate($request, $response);
