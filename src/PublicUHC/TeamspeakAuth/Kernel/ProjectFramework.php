571ede4ca41720822630ad1f53b24c6513240263
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-10 12:04:19 +0100

    base project kernel

---
+++ src/PublicUHC/TeamspeakAuth/Kernel/ProjectFramework.php
@@ -0,0 +1,6 @@
+<?php
+namespace PublicUHC\TeamspeakAuth\Kernel;
+
+use Symfony\Component\HttpKernel\DependencyInjection\ContainerAwareHttpKernel;
+
+class ProjectFramework extends ContainerAwareHttpKernel {}
