1ff94267234caebfb886d56807307950517eaf50
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-17 11:57:46 +0100

    added composer script that will copy the dist config to /config/config.yml on install/update

---
+++ src/PublicUHC/TeamspeakAuth/CopyNewConfig.php
@@ -0,0 +1,34 @@
+<?php
+namespace PublicUHC\TeamspeakAuth;
+
+use Composer\Script\Event;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Yaml\Yaml;
+
+class CopyNewConfig {
+
+    public static function postInstallCommand(Event $event)
+    {
+        $extras = $event->getComposer()->getPackage()->getExtra();
+
+        $configFileLocation = $extras['ymlinstall']['config'];
+        $distFileLocation = $extras['ymlinstall']['dist'];
+
+        //make the file if it doesn't exist
+        if(!is_file($configFileLocation)) {
+            touch($configFileLocation);
+        }
+
+        //parse the YML files
+        $configYML = Yaml::parse($configFileLocation, true);
+        $distYML = Yaml::parse($distFileLocation, true);
+
+        //TODO ask for each parameter that doesn't exist and only copy those that don't exist
+        $configYML['parameters'] = $distYML['parameters'];
+
+        //copy the services across directly
+        $configYML['services'] = $distYML['services'];
+
+        file_put_contents($configFileLocation, Yaml::dump($configYML, 3, 2));
+    }
+} 
