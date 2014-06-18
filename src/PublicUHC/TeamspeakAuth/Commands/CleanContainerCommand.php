ad7f241712157e5dfce658400ac056558752a8f6
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-18 16:49:20 +0100

    make a generic folder remover parent class

--- src/PublicUHC/TeamspeakAuth/Commands/CleanContainerCommand.php
+++ src/PublicUHC/TeamspeakAuth/Commands/CleanContainerCommand.php
@@ -1,15 +1,7 @@
 <?php
 namespace PublicUHC\TeamspeakAuth\Commands;
 
-
-use FilesystemIterator;
-use RecursiveDirectoryIterator;
-use RecursiveIteratorIterator;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
-
-class CleanContainerCommand extends Command {
+class CleanContainerCommand extends CleanCommand {
 
     protected function configure()
     {
@@ -17,18 +9,11 @@ class CleanContainerCommand extends Command {
             ->setDescription('Cleans the cached container (to be ran after config changes)');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /**
+     * @return String path to the folder to clean
+     */
+    protected function getFolder()
     {
-        $base_folder = 'cache/container';
-
-        $output->writeln("<info>Removing folder $base_folder</info>");
-        $directoryIterator = new RecursiveDirectoryIterator($base_folder, FilesystemIterator::SKIP_DOTS);
-        $iterator = new RecursiveIteratorIterator($directoryIterator, RecursiveIteratorIterator::CHILD_FIRST);
-        foreach($iterator as $path) {
-            $path->isDir() ? rmdir($path->getPathname()) : unlink($path->getPathname());
-        }
-        rmdir($base_folder);
-
-        $output->writeln('<comment>Container cleared!</comment>');
+        return '/cache/container';
     }
-} 
+}
