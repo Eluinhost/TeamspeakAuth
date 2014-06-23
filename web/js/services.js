168aa87be05ab8f866a221a593481283db3c2297
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-23 16:29:40 +0100

    use correct path

--- web/js/services.js
+++ web/js/services.js
@@ -2,5 +2,5 @@ var teampseakAuthServices = angular.module('teamspeakAuthServices', ['ngResource
 
 teampseakAuthServices.factory('LatestAuthsService', ['$resource',
     function($resource){
-        return $resource('/api/v1/authentications/latest');
+        return $resource('/api/v1/authentications');
     }]);
