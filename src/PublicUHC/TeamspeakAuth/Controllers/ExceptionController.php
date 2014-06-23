6e303046a3ed6f896394a289b420fb15f18a6744
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-23 14:43:33 +0100

    change exception handler to use a single method + template

--- src/PublicUHC/TeamspeakAuth/Controllers/ExceptionController.php
+++ src/PublicUHC/TeamspeakAuth/Controllers/ExceptionController.php
@@ -3,24 +3,55 @@ namespace PublicUHC\TeamspeakAuth\Controllers;
 
 use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class ExceptionController extends ContainerAware {
 
     public function onException(FlattenException $exception) {
-        $methodName = 'on' . $exception->getStatusCode() . 'CodeException';
-        if(!method_exists($this, $methodName)) {
-            $methodName = 'on500CodeException';
+        $format =  $this->container->get('request')->getRequestFormat();
+
+        $errorCode = $exception->getStatusCode();
+
+        $errorMessage = 'Unknown Error';
+        $errorType = 'Unknown Error';
+        switch($errorCode) {
+            case 400:
+                $errorMessage = $exception->getMessage();
+                $errorType = 'Bad Request';
+                break;
+            case 403:
+                $errorMessage = 'Access Denied';
+                $errorType = 'Unauthorized';
+                break;
+            case 404:
+                $errorMessage = 'File Not Found';
+                $errorType = 'Not Found';
+                break;
+            case 500:
+                $errorMessage = 'Internal Server Error';
+                $errorType = 'Internal Server Error';
+                break;
         }
-        return $this->{$methodName}($exception);
-    }
 
-    public function on500CodeException(FlattenException $exception) {
-        return new Response($this->container->get('templating')->render('500.html.haml'));
+        switch($format) {
+            case 'json':
+                return new JsonResponse([
+                    'ERROR' => $errorMessage
+                ], $errorCode);
+            default:
+                $errorCodeHtml = str_replace('0', '<i class="fa fa-times-circle"></i>', $errorCode);
+                return new Response(
+                    $this->container->get('templating')->render(
+                        'error.html.haml',
+                        [
+                            'errorMessage'  => $errorMessage,
+                            'errorCode'     => $errorCode,
+                            'errorCodeHtml'     => $errorCodeHtml,
+                            'errorType'     => $errorType
+                        ]
+                    )
+                );
+        }
     }
-
-    public function on404CodeException(FlattenException $exception) {
-        return new Response($this->container->get('templating')->render('404.html.haml'));
-    }
-
 } 
