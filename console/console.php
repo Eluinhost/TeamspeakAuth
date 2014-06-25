334929b86782f9a61114b6e7bcb3778e6709ab88
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-25 16:19:17 +0100

    convert console to use a symfony style

--- console/console.php
+++ console/console.php
@@ -1,77 +1,23 @@
 <?php
 
-use PublicUHC\TeamspeakAuth\Commands\CleanCacheCommand;
-use PublicUHC\TeamspeakAuth\Commands\CleanContainerCommand;
-use PublicUHC\TeamspeakAuth\Commands\CleanRoutingCommand;
-use PublicUHC\TeamspeakAuth\Commands\CleanSkinsCommand;
-use PublicUHC\TeamspeakAuth\Commands\CleanTemplatesCommand;
-use PublicUHC\TeamspeakAuth\Commands\UpdateConfigCommand;
-use PublicUHC\TeamspeakAuth\Container\ProjectContainer;
-use Symfony\Bridge\ProxyManager\LazyProxy\Instantiator\RuntimeInstantiator;
-use Symfony\Bridge\ProxyManager\LazyProxy\PhpDumper\ProxyDumper;
-use Symfony\Component\Config\ConfigCache;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Console\Application;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use PublicUHC\TeamspeakAuth\Kernel\ProjectApplication;
+use PublicUHC\TeamspeakAuth\Kernel\ProjectFramework;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$isDebug = false;
-$projectRoot = dirname(dirname(__FILE__));
+set_time_limit(0);
 
-$containerFile = $projectRoot . '/cache/container/ProjectContainer.php';
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Debug\Debug;
 
-$application = null;
+$input = new ArgvInput();
+$env = $input->getParameterOption(array('--env', '-e'), getenv('SYMFONY_ENV') ?: 'dev');
+$debug = getenv('SYMFONY_DEBUG') !== '0' && !$input->hasParameterOption(array('--no-debug', '')) && $env !== 'prod';
 
-try {
-    $containerConfigCache = new ConfigCache($containerFile, $isDebug);
-
-    if (!$containerConfigCache->isFresh()) {
-        $container = new ContainerBuilder();
-        $container->setParameter('global.root', $projectRoot);
-
-        $container->setProxyInstantiator(new RuntimeInstantiator());
-
-        $configLocator = new FileLocator($projectRoot . '/config/');
-        $diLoader = new YamlFileLoader($container, $configLocator);
-        $diLoader->load('config.yml');
-
-        $container->compile();
-
-        $dumper = new PhpDumper($container);
-        $dumper->setProxyDumper(new ProxyDumper());
-        $containerConfigCache->write(
-            $dumper->dump([
-                'class' => 'ProjectContainer',
-                'namespace' => 'PublicUHC\TeamspeakAuth\Container'
-            ]),
-            $container->getResources()
-        );
-    }
-
-    require_once $containerFile;
-
-    $projectContainer = new ProjectContainer();
-
-    $application = $projectContainer->get('console_application');
-} catch (Exception $ex) {
-    echo $ex->getMessage()."\n";
-    echo "Exception building project container, only configuration command will be available\n";
+if ($debug) {
+    Debug::enable();
 }
 
-if($application == null) {
-    $application = new Application();
-}
-
-//load commands without DI needs
-
-$application->add(new UpdateConfigCommand());
-$application->add(new CleanContainerCommand($projectRoot));
-$application->add(new CleanRoutingCommand($projectRoot));
-$application->add(new CleanTemplatesCommand($projectRoot));
-$application->add(new CleanSkinsCommand($projectRoot));
-$application->add(new CleanCacheCommand($projectRoot));
-
-$application->run();
+$kernel = new ProjectFramework($env, $debug);
+$application = new ProjectApplication($kernel);
+$application->run($input);
