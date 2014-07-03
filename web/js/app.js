50c78f41f526f457e40224b56c4cd51d482267d8
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-03 10:04:34 +0100

    load the index route by default

--- web/js/app.js
+++ web/js/app.js
@@ -1,9 +1,32 @@
-angular.module('teamspeakAuthApp', ['mm.foundation'])
+angular.module('teamspeakAuthApp', ['mm.foundation', 'ui.router'])
 
+/*************************************
+ *  Configure the application routes *
+ *************************************/
+    .config(function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+
+            .state('index', {
+                url: '/',
+                templateUrl: 'partials/index'
+            })
+
+        // catch all route
+        // send users to the form page
+        $urlRouterProvider.otherwise('/');
+    })
+
+/************************
+ *  Add our controllers *
+ ************************/
     .controller('LatestAuthsCtrl', ['$scope', 'LatestAuthsService', function ($scope, LatestAuthsService) {
         $scope.latest = LatestAuthsService.query();
     }])
 
+/***************************************
+ *  Configure the application services *
+ ***************************************/
     .factory('LatestAuthsService', ['$resource', function($resource){
         var URL = NgRouting.generateResourceUrl('api_v1_authentications_all');
         return $resource( URL, {_format: 'json'});
