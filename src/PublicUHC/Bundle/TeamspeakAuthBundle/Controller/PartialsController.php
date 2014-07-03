0f2f31046605b15ef7c41c261ab6de2cfb1f8822
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-03 09:20:46 +0100

    add a controller for /partials/{name} that will render the .html.haml partial files

---
+++ src/PublicUHC/Bundle/TeamspeakAuthBundle/Controller/PartialsController.php
@@ -0,0 +1,27 @@
+<?php
+namespace PublicUHC\Bundle\TeamspeakAuthBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PartialsController extends Controller {
+
+    /**
+     * Render a partial for angular
+     *
+     * @Route("/partials/{name}")
+     *
+     * @param $name string the name of the partial to render
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException if template not found
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function renderPartialAction($name)
+    {
+        try {
+            return $this->render("PublicUHCTeamspeakAuthBundle:Partials:$name.html.haml");
+        } catch (\InvalidArgumentException $ex) {
+            throw $this->createNotFoundException();
+        }
+    }
+} 
