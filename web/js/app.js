8a58ad03628c5fe9c99fad90d3f3624fae479a7e
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-03 09:56:46 +0100

    no need to go so ham on file structure, only a small app

--- web/js/app.js
+++ web/js/app.js
@@ -1,1 +1,18 @@
-var teamspeakAuthApp = angular.module('teamspeakAuthApp', ['teamspeakAuthControllers', 'teamspeakAuthServices', 'mm.foundation']);
+angular.module('teamspeakAuthApp', ['mm.foundation'])
+
+    .controller('LatestAuthsCtrl', ['$scope', 'LatestAuthsService', function ($scope, LatestAuthsService) {
+        $scope.latest = LatestAuthsService.query();
+    }])
+
+    .factory('LatestAuthsService', ['$resource', function($resource){
+        var URL = NgRouting.generateResourceUrl('api_v1_authentications_all');
+        return $resource( URL, {_format: 'json'});
+    }])
+
+    .factory('RequestTeamspeakCodeService', ['$resource', function($resource) {
+        var URL = NgRouting.generateResourceUrl('api_v1_teamspeak_code_request');
+        return $resource( URL, {_format: 'json'}, {'update': { method:'PUT'}});
+    }]);
+
+
+
--- web/js/controllers.js
+++
@@ -1,5 +0,0 @@
-var teamspeakAuthControllers = angular.module('teamspeakAuthControllers', []);
-
-teamspeakAuthControllers.controller('LatestAuthsCtrl', ['$scope', 'LatestAuthsService', function ($scope, LatestAuthsService) {
-    $scope.latest = LatestAuthsService.query();
-}]);
