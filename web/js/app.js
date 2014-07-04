ec72054fc0ee800a63828c000fea988d8947dec6
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-04 10:45:19 +0100

    directive to bind on particular keypresses

--- web/js/app.js
+++ web/js/app.js
@@ -102,6 +102,21 @@ angular.module('teamspeakAuthApp', ['mm.foundation', 'ui.router', 'ngResource', 
                 }
             }
         };
-    }]);
+    }])
+
+    //directive with keybind="expression()" key=13
+    .directive('keybind', function() {
+        return function(scope, element, attrs) {
+            element.bind("keydown keypress", function(event) {
+                if(event.which === Number(attrs.key)) {
+                    scope.$apply(function(){
+                        scope.$eval(attrs.keybind);
+                    });
+
+                    event.preventDefault();
+                }
+            });
+        };
+    });
 
 
