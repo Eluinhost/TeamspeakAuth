3ae4ec6688b031e1df6a582c400466fdec75ebc8
HowdenG <graham_howden1@yahoo.co.uk>
2014-07-16 16:35:14 +0100

    update documentation + better status codes

--- src/PublicUHC/Bundle/TeamspeakAuthBundle/Controller/AuthenticationController.php
+++ src/PublicUHC/Bundle/TeamspeakAuthBundle/Controller/AuthenticationController.php
@@ -17,7 +17,10 @@ use PublicUHC\Bundle\TeamspeakAuthBundle\Entity\MinecraftCode;
 use PublicUHC\Bundle\TeamspeakAuthBundle\Entity\TeamspeakAccount;
 use PublicUHC\Bundle\TeamspeakAuthBundle\Entity\TeamspeakCode;
 use PublicUHC\Bundle\TeamspeakAuthBundle\Helpers\TeamspeakHelper;
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use TeamSpeak3_Exception;
 
 /**
@@ -32,8 +35,15 @@ class AuthenticationController extends FOSRestController {
      * @Post("/v1/authentications", name="api_v1_authentications_new")
      *
      * @ApiDoc(
+     * section="Authentication",
      * description="Add a new authentication to the system between a Teamspeak account and a Minecraft account",
-     * tags={"website"}
+     * tags={"website"},
+     * statusCodes={
+     *         200="On success",
+     *         400="On invalid parameters",
+     *         401="On invalid authentication",
+     *         503="On error reaching teamspeak"
+     *     }
      * )
      * @RequestParam(name="ts_uuid", description="Teamspeak UUID")
      * @RequestParam(name="ts_code", description="Teamspeak Code")
@@ -64,14 +74,14 @@ class AuthenticationController extends FOSRestController {
             /** @var $tsCode TeamspeakCode */
             $tsCode = $tsqb->getQuery()->getSingleResult();
         } catch (NoResultException $ex) {
-            throw new BadRequestHttpException('Invalid Teamspeak code supplied');
+            throw new UnauthorizedHttpException('Invalid Teamspeak code supplied');
         }
 
         /** @var $tsAccount TeamspeakAccount */
         $tsAccount = $tsCode->getAccount();
 
         if($tsAccount->getUUID() != $ts_uuid) {
-            throw new BadRequestHttpException('Invalid Teamspeak code supplied');
+            throw new UnauthorizedHttpException('Invalid Teamspeak code supplied');
         }
 
         $mcqb = $entityManager->createQueryBuilder();
@@ -93,14 +103,14 @@ class AuthenticationController extends FOSRestController {
             /** @var $mcCode MinecraftCode */
             $mcCode = $mcqb->getQuery()->getSingleResult();
         } catch(NoResultException $ex) {
-            throw new BadRequestHttpException('Invalid Minecraft code supplied');
+            throw new UnauthorizedHttpException('Invalid Minecraft code supplied');
         }
 
         /** @var $mcAccount MinecraftAccount */
         $mcAccount = $mcCode->getAccount();
 
         if($mcAccount->getName() != $mc_uuid) {
-            throw new BadRequestHttpException('Invalid Minecraft code supplied');
+            throw new UnauthorizedHttpException('Invalid Minecraft code supplied');
         }
 
         //ALL CODES MATCHED, RUN THE PROCESS
@@ -111,7 +121,7 @@ class AuthenticationController extends FOSRestController {
             $tsHelper->verifyClient($tsAccount, $mcAccount);
         } catch ( TeamSpeak3_Exception $ex ) {
             error_log($ex->getMessage());
-            throw new BadRequestHttpException('There was a problem contacting Teamspeak');
+            throw new ServiceUnavailableHttpException('There was a problem contacting the Teamspeak server');
         }
 
         return $this->view(null, 200);
@@ -121,11 +131,16 @@ class AuthenticationController extends FOSRestController {
      * @Get("/v1/authentications", name="api_v1_authentications_all")
      *
      * @ApiDoc(
+     * section="Authentication",
      * description="Fetch a list of all the authentications, latest first",
      * tags={"API"},
-     * output="PublicUHC\Bundle\TeamspeakAuthBundle\Entity\Authentication"
+     * output="PublicUHC\Bundle\TeamspeakAuthBundle\Entity\Authentication",
+     * statusCodes={
+     *      200="On success",
+     *      400="On invalid parameters"
+     * }
      * )
-     * @QueryParam(name="limit", description="Amount to return", requirements="\d+", default="10")
+     * @QueryParam(name="limit", description="Amount to return, max 50", requirements="\d+", default="10")
      * @QueryParam(name="offset", description="Offset to use", requirements="\d+", default="0")
      */
     public function api_v1_allAction($limit, $offset)
